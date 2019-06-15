87b141eb6b77f2bdfdbc21fad26b2d667ec2c3fe
HoffsMH <matthecker@pm.me>
2019-06-15 09:45:45 -0500

    lots o new

---
+++ linux/pacman_basics.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+pacman -S --noconfirm \
+       fzf \
+       fasd \
+       jq \
+       firefox \
+       neofetch \
+       openssh \
+       sudo \
+       networkmanager \
+       grub \
+       figlet \
+       zsh \
+       the_silver_searcher
--- linux/run.sh
+++ linux/run.sh
@@ -18,29 +18,38 @@
 # mount /dev/sda4 /mnt/boot
 # mount /dev/sda2 /mnt/home
 
+set -e
+
 source "./config.sh"
+
+echo "###############################################"
+echo "RUNNING PACSCTRAP"
+echo "###############################################"
 
 ./pacstrap.sh
 
+echo "###############################################"
+echo "CHROOTING"
+echo "###############################################"
+
 arch-chroot /mnt
 
-pacman -S --noconfirm \
-       fzf \
-       fasd \
-       jq \
-       firefox \
-       neofetch \
-       openssh \
-       sudo \
-       networkmanager \
-       grub \
-       figlet \
-       zsh \
-       the_silver_searcher
+echo "###############################################"
+echo "PACMAN BASIC RECIPE"
+echo "###############################################"
 
+./pacman_basics.sh
+
+echo "###############################################"
+echo "CREATING USER"
+echo "###############################################"
 
 adduser -m -g wheel $USERNAME
 passwd $USERNAME
+
+echo "###############################################"
+echo "CREATING DIRS"
+echo "###############################################"
 
 mkdir -p "$USER_HOME/code/util/aur" \
          "$USER_HOME/code/util/" \
