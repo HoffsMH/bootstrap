ebeb52e3aea20b0caa3636c3a4f207979e95ebb7
HoffsMH <matthecker@pm.me>
2019-06-23 18:59:10 -0500

    maaan this is such an awkward middle pont

--- common/link_recipe.sh
+++ common/link_recipe.sh
@@ -7,9 +7,9 @@ echo "COMMON LINKS"
 echo "###############################################"
 
 ln -sf ~/personal/dotfiles/.spacemacs ~/.
-ln -sf ~/personal/dotfiles/.gitignore_global ~/.
-ln -sf ~/personal/dotfiles/.gitconfig ~/.
-ln -sf ~/personal/dotfiles/ssh/config ~/.ssh/config
+ln -sf ~/personal/dotfiles/common/.gitignore_global ~/.
+ln -sf ~/personal/dotfiles/common/.gitconfig ~/.
+ln -sf ~/personal/dotfiles/common/ssh/config ~/.ssh/config
 ln -sf ~/personal/dotfiles/common/.fzf.sh ~/.
 
 ln -sf ~/code/util/oh-my-zsh ~/.oh-my-zsh
