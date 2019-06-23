ecb96841226659cf730405f6c22880ae437d85b8
HoffsMH <matthecker@pm.me>
2019-06-23 18:56:04 -0500

    beginnings of a common  folder in dotfiles

--- common/link_recipe.sh
+++ common/link_recipe.sh
@@ -10,6 +10,7 @@ ln -sf ~/personal/dotfiles/.spacemacs ~/.
 ln -sf ~/personal/dotfiles/.gitignore_global ~/.
 ln -sf ~/personal/dotfiles/.gitconfig ~/.
 ln -sf ~/personal/dotfiles/ssh/config ~/.ssh/config
+ln -sf ~/personal/dotfiles/common/.fzf.sh ~/.
 
 ln -sf ~/code/util/oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/code/util/spacemacs ~/.emacs.d
