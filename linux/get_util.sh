1da4dafda0c73fca4d1ebee6a4cc37b64513c83c
HoffsMH <matthecker@pm.me>
2019-06-23 16:48:02 -0500

    go

--- linux/get_util.sh
+++ linux/get_util.sh
@@ -10,14 +10,20 @@ echo "###############################################"
 
 pushd "$HOME/code/util"
 
+rm -rf oh-my-zsh
+rm -rf spaceship-prompt
+rm -rf exenv
+rm -rf zsh-autopair
+rm -rf spacemacs
+rm -rf zsh-syntax-highlighting
+
 git clone git://github.com/robbyrussell/oh-my-zsh.git
 git clone git://github.com/denysdovhan/spaceship-prompt.git
 git clone git://github.com/mururu/exenv.git
 git clone git://github.com/hlissner/zsh-autopair.git
 git clone git://github.com/syl20bnr/spacemacs.git
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
-git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/st
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/surf
 
---
+++ linux/link_recipe.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -e
+
+echo "###############################################"
+echo "COMMON LINKS"
+echo "###############################################"
+
+ln -sf ~/personal/dotfiles/linux/i3_config ~/.config/i3/config
+ln -sf ~/personal/dotfiles/linux/kitty.conf ~/.config/kitty/kitty.conf
