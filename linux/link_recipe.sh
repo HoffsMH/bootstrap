96f142729b0740c573ede6578491137736ba94bf
HoffsMH <matthecker@pm.me>
2019-06-23 19:07:46 -0500

    more linking recipes

--- linux/link_recipe.sh
+++ linux/link_recipe.sh
@@ -8,3 +8,6 @@ echo "###############################################"
 
 ln -sf ~/personal/dotfiles/linux/i3_config ~/.config/i3/config
 ln -sf ~/personal/dotfiles/linux/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf ~/personal/dotfiles/linux/.xinitrc ~/.xinitrc
+ln -sf ~/personal/dotfiles/linux/.Xmodmap ~/.Xmodmap
+ln -sf ~/personal/dotfiles/linux/.profile ~/.profile
