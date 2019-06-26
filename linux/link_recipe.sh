8cd53eb9a4d5834a6957fa59dee386b321103298
HoffsMH <matthecker@pm.me>
2019-06-25 22:25:10 -0500

    install gnome script and zprofile instead of .profile

--- linux/link_recipe.sh
+++ linux/link_recipe.sh
@@ -13,6 +13,6 @@ ln -sf ~/personal/dotfiles/linux/i3_config ~/.config/i3/config
 ln -sf ~/personal/dotfiles/linux/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/personal/dotfiles/linux/.xinitrc ~/.xinitrc
 ln -sf ~/personal/dotfiles/linux/.Xmodmap ~/.Xmodmap
-ln -sf ~/personal/dotfiles/linux/.profile ~/.profile
+ln -sf ~/personal/dotfiles/linux/.profile ~/.zprofile
 ln -sf ~/personal/dotfiles/linux/.zshrc ~/.zshrc
 ln -sf ~/personal/dotfiles/linux/.spacemacs ~/.spacemacs
