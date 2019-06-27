b8cd601fc744295b2b8003115cf7d80a456e8985
HoffsMH <matthecker@pm.me>
2019-06-27 17:37:53 -0500

    added lfrc to setup

--- linux/link_recipe.sh
+++ linux/link_recipe.sh
@@ -18,3 +18,4 @@ ln -sf ~/personal/dotfiles/linux/.profile ~/.zprofile
 ln -sf ~/personal/dotfiles/linux/.zshrc ~/.zshrc
 ln -sf ~/personal/dotfiles/linux/.spacemacs ~/.spacemacs
 ln -sf ~/personal/dotfiles/linux/conky.conf ~/.config/conky/conky.conf
+ln -sf ~/personal/dotfiles/linux/lfrc ~/.config/lf/lfrc
